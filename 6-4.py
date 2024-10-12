e77036af264fe9110d7f68622327f05148b59e43
Ilya <pankovskiy.im@gmail.com>
2024-10-12 20:25:55 +0300

    task 6-4

--- 6-4.py
+++ 6-4.py
@@ -1,8 +1,21 @@
 """
 """
 
-def correction_permutation(number, array):
-    return (2, '1 3 2 4')
+def correction_permutation(len_array, numbers):
+    numbers = list(map(int, numbers.split()))
+    all_permutation = set(range(1, len_array + 1))
+    s1 = set(numbers)
+    all_permutation -= s1
+    repeat = set()
+    count = 0
+    for i in range(len_array):
+        if numbers[i] > len_array or numbers[i] in repeat:
+            numbers[i] = all_permutation.pop()
+            count += 1
+        repeat.add(numbers[i])
+    return (count, ' '.join(map(str, numbers)))
+
+
 
 tasks = {
     (4, '1 100 3 3'): (2, '1 2 3 4'),
