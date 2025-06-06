94421f550d65b9438593892c25df7d1b55c008e3
Ilya <pankovskiy.im@gmail.com>
2025-06-06 14:26:40 +0300

    correct solution

--- binary_search.py
+++ binary_search.py
@@ -1,16 +1,16 @@
-def binary_search(list, item):
+def binary_search(list, target):
     low = 0
-    high = len(list) - 1
-    while low <= high: 
-        mid = (low + high) // 2 
-        guess = list[mid]
-        if guess == item: 
+    right = len(list) - 1
+    while low <= right: 
+        mid = (low + right) // 2 
+        number = list[mid]
+        if number == target: 
             return mid
-        elif guess > item:
-            high = mid - 1
+        elif number > target:
+            right = mid - 1
         else: 
             low = mid + 1
-            return None 
+    return None 
 my_list = [1, 3, 5, 7, 9] 
 print(binary_search(my_list, 3)) # => 1
 print(binary_search(my_list, -1)) # => None
